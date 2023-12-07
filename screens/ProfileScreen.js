d0e338fe657546767399d26107f6d715a7d55070
Shehad Uj Jahan <143080536+shehad-404@users.noreply.github.com>
2023-12-07 12:33:05 +0600

    Add files via upload

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text
+        style={{
+            marginTop: 50,
+            marginLeft: 20,
+            fontWeight: "bold",
+            color: "black",
+            fontSize: 15,
+          }}
+      >
+      Profile</Text>
+    </View>
+  )
+}
+
+export default ProfileScreen
+
+const styles = StyleSheet.create({})
---
+++ screens/SavedScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SavedScreen = () => {
+  return (
+    <View>
+      <Text 
+      style={{
+            marginTop: 50,
+            marginLeft: 20,
+            fontWeight: "bold",
+            color: "black",
+            fontSize: 15,
+          }}
+          >
+          Saved Items</Text>
+    </View>
+  )
+}
+
+export default SavedScreen
+
+const styles = StyleSheet.create({})
